1060b99a84a10266ae2e1e22eedd68783e100bcf
alinass73 <97101249+alinass73@users.noreply.github.com>
2024-11-23 14:51:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // console.log(/)
 // import TimeLineSlider from 'timeline-slider'; 
-import TimeLineSlider from '../TimeLineSlider.js';
+import TimeLineSlider from './TimeLineSlider.js';
 
 // import 'timeline-slider/css';
 
